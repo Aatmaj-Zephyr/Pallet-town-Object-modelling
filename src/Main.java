736d278a73fe99ef9f08ba36bdb5c6b407f623c4
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-07 12:17:08 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Pokemon a=new Charmander();
+		System.out.println("I am a "+a.text);
+	//	System.out.println(a.name);
+	//	a.set_name("Flamy");
+	//	System.out.println(a.text);
+	//	System.out.println(a.type1.text);
+	//	System.out.println(a.stats.get_hp());
+		//System.out.println(a.base.get_hp());
+	//	a=a.evolve();
+	}
+}

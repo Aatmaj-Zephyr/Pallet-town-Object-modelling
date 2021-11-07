648d25a3f7e5139d001ed186819ce3a5dea4fc39
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-07 12:20:47 +0530

    Create Base.java

---
+++ src/Base.java
@@ -0,0 +1,10 @@
+public class Base extends Properties{
+    private int hp;
+    private int speed;
+    private int attack;
+    private int spattack;
+    public Base(int hp,int speed,int attack,int spattack){
+     //base stats
+     super(hp,speed,attack,spattack);
+    }
+}

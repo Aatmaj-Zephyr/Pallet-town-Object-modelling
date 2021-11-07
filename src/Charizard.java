7f09c7204e11195b9dc45e3a64f1126a7f1a5203
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-07 13:12:35 +0530

    Update Charizard.java

--- src/Charizard.java
+++ src/Charizard.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 public class Charizard extends Pokemon {
     Charizard(){
-        base=new Base(110,50,20,50);
-        stats=new Stats(base);
+        this.base=new Base(110,50,20,50);
+        this.stats=new Stats(base);
         //stats iterable todo
-    String name="Charizard";
-    String text="Charizard";
-    Type type1 = new Fire();
-    Type type2 =new Flying();
-    ArrayList<Move> My_Moves = new ArrayList<Move>(); // Create an ArrayList object 
+    this.name="Charizard";
+    this.text="Charizard";
+    this.type1 = new Fire();
+    this.type2 =new Flying();
+    this.My_Moves = new ArrayList<Move>(); // Create an ArrayList object 
     }
     Charizard(Pokemon PreEvolution){
         super(PreEvolution);

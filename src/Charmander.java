fce6e54273d70b9d4237e5c4e5401e40eb665a0e
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-07 13:08:42 +0530

    Update Charmander.java

--- src/Charmander.java
+++ src/Charmander.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
 public class Charmander extends Pokemon {
     Charmander(){
-        base=new Base(90,30,20,40);
-        stats=new Stats(base);
+        this.base=new Base(90,30,20,40);
+        this.stats=new Stats(base);
         //stats iterable todo
-    String name="Charmander";
-    String text="Charmander";
-    Type type1 =new Fire();
-    Type type2 =new Fire();
-    Pokemon PostEvolution = new Charmelon(this);
-    ArrayList<Move> My_Moves = new ArrayList<Move>(); // Create an ArrayList object 
+    this.name="Charmander";
+    this.text="Charmander";
+    this.type1 =new Fire();
+    this.type2 =new Fire();
+    this.PostEvolution = new Charmelon(this);
+    this.My_Moves = new ArrayList<Move>(); // Create an ArrayList object 
    
     }
 

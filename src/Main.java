c8f9f567e68c984579af5bdc3cdcc0e40e592a68
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-07 13:06:08 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,17 @@ public class Main
 		System.out.println("Hello World");
 		Pokemon a=new Charmander();
 		System.out.println("I am a "+a.text);
-	//	System.out.println(a.name);
-	//	a.set_name("Flamy");
-	//	System.out.println(a.text);
-	//	System.out.println(a.type1.text);
-	//	System.out.println(a.stats.get_hp());
-		//System.out.println(a.base.get_hp());
+		System.out.println(a.name);
+		a.set_name("Flamy");
+		System.out.println("My name is "+a.name);
+	
+		
+		System.out.println(a.type1.text);
+		System.out.println(a.stats.get_hp());
+		a.stats.hp=80;
+		System.out.println(a.stats.get_hp());
+		System.out.println(a.base.get_hp());
+		
 	//	a=a.evolve();
 	}
 }

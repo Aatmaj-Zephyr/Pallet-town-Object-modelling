f68806bc454c85e3415313b376c97ae99e4bf9eb
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-13 12:13:15 +0530

    Update Flamethrower.java

--- src/Flamethrower.java
+++ src/Flamethrower.java
@@ -1,12 +1,14 @@
-public  class Flamethrower extends move{
-    //Strategy pattern
-    this.name="Flamethropwer"; //name of the move
+public  class Flamethrower extends Move{
+    Flamethrower(){
+    this.name="Flamethrower"; //name of the move
     this.Move_type=new fire(); //type of the Move_type
     this.probability=50;//probability of the moveeffect
     this.acccuracy=80;//accuracy of the move being hit.
+    this.Damage=80;
+    }
     void effect(Pokemon opponent){//effect of the move
-    opponent.status= new Burn()
+    opponent.status= new Burn();
         
     }
-    this.Damage=80;
+    
 }

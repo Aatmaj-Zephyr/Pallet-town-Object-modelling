dacbf260b925f899de262292bff93c716ece5edf
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-13 11:55:36 +0530

    Update Move.java

--- src/Move.java
+++ src/Move.java
@@ -1,6 +1,9 @@
 public abstract class Move{
     //Strategy pattern
-    String name;
-    Type Move_type;
+    String name; //name of the move
+    Type Move_type; //type of the Move_type
+    int probability;//probability of the moveeffect
+    int acccuracy;//accuracy of the move being hit.
+    void effect(Pokemon opponent);//effect of the move
     int Damage;
 }

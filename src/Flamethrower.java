90e4126b199780a30da1001663850a2e2475d71c
Aatmaj <Aatmaj.mhatre@gmail.com>
2021-11-13 12:15:30 +0530

    Update Flamethrower.java

--- src/Flamethrower.java
+++ src/Flamethrower.java
@@ -1,7 +1,7 @@
 public  class Flamethrower extends Move{
     Flamethrower(){
     this.name="Flamethrower"; //name of the move
-    this.Move_type=new fire(); //type of the Move_type
+    this.Move_type=new Fire(); //type of the Move_type
     this.probability=50;//probability of the moveeffect
     this.acccuracy=80;//accuracy of the move being hit.
     this.Damage=80;
